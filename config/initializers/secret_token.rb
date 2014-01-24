07b98674054b78a9946fabcfee5715f7e8bd7112
Ruben Epong <rubenjohne@gmail.com>
2014-01-24 13:46:41 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grinch::Application.config.secret_key_base = 'ef0090bc790247991c89447f1d9224fa2a3b78d3aad11c41cdb125cd31f67c6133edda86b1a9f596311fa4abb9f43c681884dea5b403a2e4f27850265443db9c'
